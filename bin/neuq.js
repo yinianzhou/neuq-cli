44a1178bfeab1205ad9ad6e0de85eab0998877fc
周奕年 <zhouyn@neuqsoft.com>
2022-07-20 16:00:02 +0800

    add

---
+++ bin/neuq.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+
+const program = require('commander');
+const chalk = require('chalk');
+const create = require('../lib/create');
+
+program.version(require('../package').version, '-v, --version').usage('<command> [options]');
+
+program
+  .command('neuq c')
+  .description('create what you want to create')
+  .action(() => {
+    create();
+  });
+
+program.parse(process.argv);
+
+if (program.args.length === 0) {
+  console.log(chalk.red('syntax error'));
+  program.help();
+}
